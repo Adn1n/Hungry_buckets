eb6bcf977be8dcc8277b6ece42f1760f2685ecea
Adn1n <adnanmoubarac@icloud.com>
2025-03-20 16:35:22 +0100

    Teddy

--- game.py
+++ game.py
@@ -1,13 +1,39 @@
 import pygame
 
 
-
 class game :
     def __init__(self):
         self.longueur, self.largeur = 850,600
         self.ecran = pygame.display.set_mode((self.largeur,self.largeur))
-        self.titre = pygame.display.set_caption("Projet Transverse")
+        pygame.display.set_caption("Projet Transverse")
+        icone = pygame.image.load('tombereau.png')
+        pygame.display.set_icon(icone)
+        self.clock = pygame.time.Clock()
+
+    def draw(self):
+        self.ecran.fill((0,0,230))
+        pygame.display.update()
+
+    def handling(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+        return True
+
+    def run(self):
+        self.running = True
+        while self.running :
+            self.handling()
+            self.draw()
+            self.clock.tick(60)
 
 
-        pass
+    pygame.quit()
+
+
+game = game()
+game.run()
 
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import time
+import pygame
+from game import game
 
+
+
+pygame.init()
+game = game()
+game.run()
